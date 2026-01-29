ebd7aa38eafa4dc039948c57016cd10e71a73680
Obed Ovabor <obedovabor@gmail.com>
2026-01-29 22:57:47 +0100

    feat: Configure PrismaClient to use `accelerateUrl` from `DATABASE_URL`.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,7 +4,9 @@ declare global {
     var prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma ?? new PrismaClient()
+export const db = globalThis.prisma ?? new PrismaClient({
+    accelerateUrl: process.env.DATABASE_URL,
+})
 
 if (process.env.NODE_ENV !== "production") {
     globalThis.prisma = db
